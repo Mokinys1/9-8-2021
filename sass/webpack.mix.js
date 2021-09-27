20df6c0d9514bdff8c6bf3d3654293fb6bc06f4a
vilius007@gmail.com <vilius007@gmail.com>
2021-09-28 01:38:02 +0300

    bootstrap

---
+++ sass/webpack.mix.js
@@ -0,0 +1,15 @@
+
+const mix = require('laravel-mix');
+
+mix.setPublicPath('public');
+
+// mix.version();
+
+// if (mix.inProduction()) {
+// 	mix.sourceMaps();
+// }
+// 
+// 
+
+mix.sass('src/sass/app.scss', 'public/css/app.css');
+// mix.sass('src/sass/_spacelab.scss', 'public/css/_spacelab.css');
