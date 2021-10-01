0d6397f5d35853b0c4698d32dcdce9ddb0b6e1e9
vilius007@gmail.com <vilius007@gmail.com>
2021-10-01 10:56:11 +0300

    test

--- JS/2021-09-29/Menu/assets/js/app-DOMContentLoaded.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector('.navbar__toggle-btn').addEventListener('click', () => {
-		document.querySelector('.navbar__menu-items').classList.toggle('active');
-	});
-});
-
-const testVariable = 5;
