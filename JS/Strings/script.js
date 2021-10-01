42786b37cfc015740e92f29850ce60fa7079d901
Vilius Vysniauskas <87119998+Mokinys1@users.noreply.github.com>
2021-10-01 11:19:01 +0300

    Delete JS/Strings directory

--- JS/Strings/script.js
+++
@@ -1,3 +0,0 @@
-let userInfoOutput = document.getElementById("main");
-let user = {firstName:"John", lastName:"Doe"};
- userInfoOutput.innerHTML += `<h2>User information</h2><p>Vartotojo vardas yra ${user.firstName} o pavarde ${user.lastName}</p>`;
