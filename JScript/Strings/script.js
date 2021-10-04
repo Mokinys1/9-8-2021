75d2ca5db9537f51c4ec584a91fe2e1bb27128c8
Vilius Vysniauskas <87119998+Mokinys1@users.noreply.github.com>
2021-10-04 18:43:05 +0300

    Add files via upload

--- JScript/Strings/script.js
+++ JScript/Strings/script.js
@@ -1,11 +1,14 @@
 let userInfoOutput = document.getElementById("main");
 let user = {firstName:"Name", lastName:"Surename"};
- userInfoOutput.innerHTML += `<h2>User information</h2><p>Vartotojo vardas yra ${user.firstName} o pavarde ${user.lastName}</p>`;
+
+ userInfoOutput.innerHTML = `<h2>User information</h2><p>Vartotojo vardas yra ${user.firstName} o pavarde ${user.lastName}</p>`;
+
 
 function buttFunc() {
 let userInfoOutput = document.getElementById("createInfo");
 let user = {firstName:"Name", lastName:"Surename"};
-userInfoOutput.innerHTML += `<h2>User information</h2><p>Vartotojo vardas yra ${user.firstName} o pavarde ${user.lastName}</p>`;
+
+userInfoOutput.innerHTML = `<h2>User information</h2><p>Vartotojo vardas yra ${user.firstName} o pavarde ${user.lastName}</p>`;
 }
 
 document.getElementById("butt").onclick=function(){
