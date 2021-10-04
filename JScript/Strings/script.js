835ff6436c1b35f7296eb0d8804a309db8495141
vilius007@gmail.com <vilius007@gmail.com>
2021-10-04 17:51:58 +0300

    JS work

--- JScript/Strings/script.js
+++ JScript/Strings/script.js
@@ -1,3 +1,13 @@
 let userInfoOutput = document.getElementById("main");
-let user = {firstName:"John", lastName:"Doe"};
+let user = {firstName:"Name", lastName:"Surename"};
  userInfoOutput.innerHTML += `<h2>User information</h2><p>Vartotojo vardas yra ${user.firstName} o pavarde ${user.lastName}</p>`;
+
+function buttFunc() {
+let userInfoOutput = document.getElementById("createInfo");
+let user = {firstName:"Name", lastName:"Surename"};
+userInfoOutput.innerHTML += `<h2>User information</h2><p>Vartotojo vardas yra ${user.firstName} o pavarde ${user.lastName}</p>`;
+}
+
+document.getElementById("butt").onclick=function(){
+buttFunc();
+}
