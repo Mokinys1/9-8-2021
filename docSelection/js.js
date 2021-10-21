59406655497387eaeb85df496a9b9973b1961c2f
vilius007@gmail.com <vilius007@gmail.com>
2021-10-21 17:48:21 +0300

    querySelection

---
+++ docSelection/js.js
@@ -0,0 +1,30 @@
+// Sukurkite HTML dokumentą su lentele (table) ir forma (form). Dokumentu turi būti žemiau aptartos žymos (angl. tags) ir jų atributai.
+
+
+// Suraskite ir konsolėje atvaizduokite:
+
+
+// Lentelę su id = "age-table".
+  
+// Pirmasis <td> toje lentelėje (su id “age”).
+  
+// Visus <label> elementus lentelės viduje (jų turėtų būti 3).
+  
+// Pirmasis įvesti (<input>) į formą.
+  
+// Paskutinę įvesti (<input>) į formą.
+
+let ageTable = document.querySelector('#age-table');
+console.log(ageTable);
+
+console.log(document.querySelector('#age'));
+
+console.log(document.querySelectorAll('label'));
+
+let obj = document.querySelector('input');
+console.log(obj);
+
+
+let lastInput = document.querySelector('input:last-of-type');
+console.log(lastInput);
+
