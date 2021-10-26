30dce4c49b979ab56f72a3fbd55412836bf6a161
vilius007@gmail.com <vilius007@gmail.com>
2021-10-26 13:17:46 +0300

    update

--- JavaScript examples/createElement+appendChild+createTextNode/js.js
+++ JavaScript examples/createElement+appendChild+createTextNode/js.js
@@ -2,7 +2,9 @@
 
 // UŽDUOTIS: Sukurti jam (jo vidui) tokį markup naudojant JavaScipt:
 
-
+// ┌─┐┬ ┬┌─┐┌─┐┬┌─  ┬  ┬┌┐┌┌─┐  
+// │  ├─┤├┤ │  ├┴┐  │  ││││├┤   25
+// └─┘┴ ┴└─┘└─┘┴ ┴  ┴─┘┴┘└┘└─┘
 
 // <h2> tag’as su tekstu;
 let ch2 = document.createElement('h2');
@@ -11,7 +13,7 @@ let ch2Text = document.createTextNode('Hello im h2 tag');
 
 ch2.appendChild(ch2Text);
 
-document.body.appendChild(ch2);
+document.querySelector('#main').appendChild(ch2);
 
 // <p> tag’as su tekstu;
 let cP = document.createElement('p');
@@ -20,7 +22,7 @@ let cPtext = document.createTextNode('Hello im p tag');
 
 cP.appendChild(cPtext);
 
-document.body.appendChild(cP);
+document.querySelector('#main').appendChild(cP); //also we can use document.body.appendChild(cP)
 
 
 // <ul> tag’as, kurio viduje yra 5 <li> su paveiksliukais;
@@ -30,7 +32,7 @@ document.body.appendChild(cP);
         let li=document.createElement('li');
         li.innerHTML=`<a href="https://www.qries.com/">
         <img alt="Qries" src="https://www.qries.com/images/banner_logo.png"
-        width="150" height="70"></a>`;
+        width="150" height="70">`;
         ul.appendChild(li);
     }
     document.querySelector('#main').appendChild(ul);
@@ -42,14 +44,8 @@ let cPtext2 = document.createTextNode('Hello im p tag 2');
 
 cP2.appendChild(cPtext2);
 
-document.body.appendChild(cP2);
+document.querySelector('#main').appendChild(cP2);
 
-
-	
-
-	
-
-	
 
 	
 
