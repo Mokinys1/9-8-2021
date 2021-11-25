ece1348b88b40dfd2482463756c84a0f6dabee06
vilius007@gmail.com <vilius007@gmail.com>
2021-11-25 16:52:40 +0200

    deleted file

--- weatherApp/web.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const _= require
-
-module.exports = http.createServer(function(request, response) {
-	response.end('hello world');
-}).on('Listening', function() {
-	console.log(`Started at ${this.address().port} port`)	
-})
