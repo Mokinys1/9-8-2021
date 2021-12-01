5c87f07718f38b37442185ca34c50c056e746827
vilius007@gmail.com <vilius007@gmail.com>
2021-12-01 20:05:43 +0200

    php tasks

--- backend/PHP/Task3.php
+++ backend/PHP/Task3.php
@@ -2,9 +2,16 @@
 
 <?php 
 
-$arr=["!","Hello ","World "];
+$arr=["!",
+	"Hello ",
+	"World "
+];
 
-$r=("$arr[1]$arr[2]$arr[0]");
+$r= "<h2 style='color:Orange'>
+		 $arr[1]
+		 $arr[2]
+		 $arr[0]
+	 <h2>";
 
 echo($r);
 
