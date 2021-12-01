6df34d92cfabb5c817af159c2eee45e6e0de7fca
vilius007@gmail.com <vilius007@gmail.com>
2021-12-01 18:45:11 +0200

    php tasks

---
+++ backend/PHP/Task2.php
@@ -0,0 +1,17 @@
+<!-- Sukurkite kintamąjį $hello ir priskirkite jam reikšmę „hello“. Naudodami atskirus šios eilutės simbolius, ekrane atspausdinkite:„h“, „e“, „o“. -->
+
+<?php 
+
+$hello = 'hello';
+
+var_dump($hello[0]);
+var_dump($hello[1]);
+var_dump($hello[4]);
+
+
+
+
+
+
+
+ ?>
