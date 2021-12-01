7b6af125bd4068a2b24c5acf8a7c9b0df6b5ef55
vilius007@gmail.com <vilius007@gmail.com>
2021-12-01 19:54:04 +0200

    php tasks

---
+++ backend/PHP/Task3.php
@@ -0,0 +1,10 @@
+<!-- Sukurkite masyvą $arr iš reikšmių: '!', 'Hello', 'world'. Naudodami sukurtą masyvą priskirti reikšme 'Hello, world!' naujam kintamajam ir atspausdinkite jį ekrane. -->
+
+<?php 
+
+$arr=["!","Hello ","World "];
+
+$r=("$arr[1]$arr[2]$arr[0]");
+
+echo($r);
+
